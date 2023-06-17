36aca675076dfdd80690c30c7e8dbd35e24722f1
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-06-17 11:17:44 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,11 @@ int main(void)
 {
 	char letter = 97;
 
-	while(letter <= 122)
+	while (letter <= 122)
 	{
 		if (letter != 'q' && letter != 'e')
 			putchar(letter);
+		letter++;
 	}
 	putchar('\n');
 return (0);
