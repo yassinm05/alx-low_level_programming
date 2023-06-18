032556fbb73da4650173c5e7946a7b7d2e90cba8
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-06-18 08:11:29 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,18 +9,18 @@ int main(void)
 {
 	char letter = 48;
 
-	while (letter < 57)
+	while (letter <= 57)
 	{
 		putchar(letter);
 		letter++;
 	}
 	letter = 97;
 
-	while(letter <= 102)
+	while (letter <= 102)
 	{
 		putchar(letter);
 		letter++;
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
