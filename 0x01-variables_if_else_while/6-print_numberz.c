4581b54f31e25798e40130e6de2b3948e090aea9
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-06-18 07:40:45 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i = 48;
+    while (i < 58)
+    {
+        putchar((char) i);
+        i++;
+    }
+    putchar('\n');
+   return(0);
+}
+   
