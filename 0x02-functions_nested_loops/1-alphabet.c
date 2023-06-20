624db531fd41042ca7b9a70ba9d55e7ce8e33838
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-06-20 10:40:22 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  *
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 	char letter = 'a';
-	
 	while (letter <= 122)
 	{
 		putchar(letter);
