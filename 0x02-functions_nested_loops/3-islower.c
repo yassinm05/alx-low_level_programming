cab5c10eeca41c482f410db619ea3016501a177b
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-06-20 11:40:06 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * int _islower - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else 
+		return (0);
+}
