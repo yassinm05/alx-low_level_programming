927fabada289499d2e845b97f3ee63e4cca7d95a
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-06-20 11:32:49 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,19 +5,19 @@
  *
  * Return: Always 0 (Success)
  */
- void print_alphabet_x10 (void)
+void print_alphabet_x10(void)
 {
 	int i;
 	
-	for (i=0;i<10;i++)
+	for (i = 0 ; i < 10; i++)
 	{
 		char letter = 'a';
 	
-	while (letter <= 122)
-	{
-		putchar(letter);
-		letter++;
-       	}
-       	putchar('\n');
-       	}
+		while (letter <= 122)
+		{
+			putchar(letter);
+			letter++;
+		}
+		putchar('\n');
+	}
 }
