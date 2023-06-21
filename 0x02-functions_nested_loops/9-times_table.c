387bb8dbbabe239913c7ad3554d170f0d65ed69d
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-06-21 07:37:24 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  times_table - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table()
+{
+	for( int i=0;i<=9;i++)
+	{
+		printf("9 * %d = %d\n",i , i*9);
+	}
+}
