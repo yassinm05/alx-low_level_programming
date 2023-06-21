80998d920e0a54bb0dca1bc1edeaf9def138b51d
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-06-21 03:13:05 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_sign - Entry point
  *
  * @n :is a parameter
  *
@@ -11,17 +11,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+, ");
-		return (1);
+		return (+1);
 	}
 	else if  (n < 0)
 	{
-		printf("-, ");
 		return (-1);
 	}
 	else
 	{
-		printf("0, ");
-		return (0);
+		return (00);
 	}
 }
