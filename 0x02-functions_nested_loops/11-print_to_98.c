76305c2cc69dd6fbaf45652e968ecc4f3ee2d6ab
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-06-21 08:37:23 -0700

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  print_to_98 - Entry point
+ *
+ * @n :is a parameter
+ *
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	for (; n<=98; n++)
+		printf("%d, ",n);
+}
