8aba66e68286b5385d9f7c2295efd530433e5527
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-06-22 11:39:14 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + 48);
+		i++;
+	}
+	_putchar('\n');
+}
