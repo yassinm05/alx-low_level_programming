d0b066a0b97743754f15c4cf2d16980f96309c23
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-06-22 11:30:54 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,11 +5,11 @@
  * Return: Always 0.
  */
 
-void print_numbers(void)
+void print_numbers()
 {
 	char letter;
 
 	for (letter = 48; letter <= 57; letter++)
 		putchar(letter);
-	puthcar('\n');
+	putchar('\n');
 }
