06477af42d737700a84c292628b57958043246f8
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-06-22 11:25:50 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - check the code.
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	char letter;
+
+	for (letter = 48; letter <= 57; letter++)
+		putchar(letter);
+	puthcar('\n');
+}
