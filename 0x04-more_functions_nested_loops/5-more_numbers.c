93115df8d0340930f27be59656b662fff31165eb
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-06-22 11:47:41 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the code.
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+	putchar('\n');
+}
