8f046c88710c26072a6123d7bb831e3cbe28bb7f
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-06-22 12:10:25 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,20 @@ void more_numbers(void)
 {
 	int i;
 	int j;
+	int k;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
-			printf("%d", j);
-		_putchar('\n');
+		{
+			k = j;
+			if (j > 9)
+			{
+				printf("%d", 1);
+				k = j % 10;
+			}
+			printf("%d", k);
+		}
 	}
+	 _putchar('\n');
 }
