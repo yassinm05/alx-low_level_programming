95fcac39b450e428bc2fdc4259dd0a2af8a5694d
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-06-23 06:34:29 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -21,10 +21,11 @@ void print_diagonal(int n)
 	{
 		for (i = 1; i <= n; i++)
 		{
-			_putchar('\\');
-			_putchar('\n');
-			for (j = 0; j < i; j++)
-				_putchar(' ');
+			for (j = 0; j < n; j++)
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
 		}
 		_putchar('\n');
 	}
