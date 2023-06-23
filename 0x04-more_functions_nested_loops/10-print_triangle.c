6d88436a37a6e73e12a62e9510151afe16c8f10b
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-06-23 08:55:27 -0700

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_triangle - check the code.
  *
@@ -6,19 +7,26 @@
  *
  * Return: Always 0.
  */
+
 void print_triangle(int size)
 {
 	if (size <= 0)
+	{
 		_putchar('\n');
+	}
 	int i, j, k, space;
 
 	space = size - 1;
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < space; j++)
+		{
 			_putchar(' ');
+		}
 		for (k = 0; k < (size - space); k++)
+		{
 			_putchar('#');
+		}
 		space--;
 		_putchar('\n');
 	}
