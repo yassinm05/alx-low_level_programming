2e6fc2a8264ffbd579fabf01d53c7bca27066b99
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-06-23 05:16:17 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * mul - check the code.
+ *
+ * @n : is a param
+ *
+ * Return: Always 0.
+ */
+
+void print_diagonal(int n)
+{
+       	int i, j;
+	
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i < n; i++)
+		{
+			for(j = 0; j <= i; j++)
+				putchar(' ');
+			putchar(92);
+			putchar('\n');
+		}
+		printf("\n");
+	}
+}
