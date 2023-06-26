25a6971279c01c1eff2f9fdafbe5d48ae59afce1
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-06-26 12:36:56 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - check the code.
+ *
+ * @str : is a param
+ *
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+	int i=0;
+	while(s[i] != '\0')
+	{
+		putchar(s[i]);
+		i+=2;
+	}
+}
