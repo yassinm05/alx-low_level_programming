3f6907e06d2bef9b8ebc1228f641130bc11829de
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-06-29 19:50:08 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,29 +12,30 @@
 char *cap_string(char *str)
 {
 	int i = 0;
-
-	for (i = 0; str[i] != '\0'; i++)
+	if(str[i] >= 97 && str[i] <= 122)
 	{
-		if (i = 0 && (str[i] >= 97 && str[i] <= 122))
-				str[i] = str[i] - 32;
-		else if (str[i] == 32 ||
-				str[i] == 9 ||
-				str[i] == 10 ||
-				str[i] == 44 ||
-				str[i] == 59 ||
-				str[i] == 46 ||
-				str[i] == 33 ||
-				str[i] == 63 ||
-				str[i] == 34 ||
-				str[i] == 40 ||
-				str[i] == 41 ||
-				str[i] == 123 ||
-				str[i] == 125)
-		{
-			if (str[i + 1] >= 97 && str[i + 1] <= 122)
-			  str[i + 1] = str[i + 1] - 32;
-		}
+	    str[i] -= 32;
+	}
+	for (i = 1; str[i] != '\0'; i++)
+	{
+	    if(str[i] >= 97 && str[i] <= 122)
+	    {
+		    if(str[i - 1] == 32 ||
+				str[i - 1] == 9 ||
+				str[i - 1] == 10 ||
+				str[i - 1] == 44 ||
+				str[i - 1] == 59 ||
+				str[i - 1] == 46 ||
+				str[i - 1] == 33 ||
+				str[i - 1] == 63 ||
+				str[i - 1] == 34 ||
+				str[i - 1] == 40 ||
+				str[i - 1] == 41 ||
+				str[i - 1] == 123 ||
+				str[i - 1] == 125)
+	    
+	             str[i] -= 32;
+	    }
 	}
 	return (str);
 }
-
