ebebf1f768ea80b975c0fb02d7c387697f4d580e
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-06-30 07:53:32 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,39 +3,43 @@
 /**
  * *cap_string - check the code.
  *
- * @*str : is a param
+ * @str : is a param
  *
  * n : is a param
  *
  * Return: Always 0.
  */
+
 char *cap_string(char *str)
 {
 	int i = 0;
-	if(str[i] >= 97 && str[i] <= 122)
+
+	if (str[i] >= 97 && str[i] <= 122)
 	{
-	    str[i] -= 32;
+		str[i] -= 32;
 	}
+
 	for (i = 1; str[i] != '\0'; i++)
 	{
-	    if(str[i] >= 97 && str[i] <= 122)
-	    {
-		    if(str[i - 1] == 32 ||
-				str[i - 1] == 9 ||
-				str[i - 1] == 10 ||
-				str[i - 1] == 44 ||
-				str[i - 1] == 59 ||
-				str[i - 1] == 46 ||
-				str[i - 1] == 33 ||
-				str[i - 1] == 63 ||
-				str[i - 1] == 34 ||
-				str[i - 1] == 40 ||
-				str[i - 1] == 41 ||
-				str[i - 1] == 123 ||
-				str[i - 1] == 125)
-	    
-	             str[i] -= 32;
-	    }
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			if (str[i - 1] == 32 ||
+					str[i - 1] == 9 ||
+					str[i - 1] == 10 ||
+					str[i - 1] == 44 ||
+					str[i - 1] == 59 ||
+					str[i - 1] == 46 ||
+					str[i - 1] == 33 ||
+					str[i - 1] == 63 ||
+					str[i - 1] == 34 ||
+					str[i - 1] == 40 ||
+					str[i - 1] == 41 ||
+					str[i - 1] == 123 ||
+					str[i - 1] == 125)
+			{
+				str[i] -= 32;
+			}
+		}
 	}
 	return (str);
 }
