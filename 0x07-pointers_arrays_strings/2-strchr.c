8e3dc4513229d744bcf419f8fc9508b9f88b5241
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-07-03 08:26:37 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,7 +14,7 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i] >= '\0')
 	{
 		if (s[i] == c)
 		{
@@ -22,5 +22,5 @@ char *_strchr(char *s, char c)
 		}
 		i++;
 	}
-	return ('\0');
+	return (NULL);
 }
