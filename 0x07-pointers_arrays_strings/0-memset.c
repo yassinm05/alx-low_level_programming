2ea3c8d38316ded40cfc074198ea21b3ce9dfe67
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-07-03 05:39:20 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_memset - check the code.
+ *
+ * @s : is a param
+ *
+ * @b : is a param
+ *
+ * @n : is a param
+ *
+ * Return: Always 0.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = 'b';
+	}
+	return (s);
+}
