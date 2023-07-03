3577e8f12683a1aff6cae5cc5970d7e2f50e7dde
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-07-03 10:27:02 -0700

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * *_strstr - check the code.
+ *
+ * @haystack : is a param
+ *
+ * @needle : is a param
+ *
+ * Return: Always 0.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+	int count = 0;
+
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		while (needle[j] != '\0')
+		{
+			count = 0;
+			while (haystack[i] != 32 && haystack[i + count] == needle[j]
+					&& needle[j] != '\0')
+			{
+				count++;
+				j++;
+			}
+			if (needle[j] == '\0')
+			{
+				return (haystack + i);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
