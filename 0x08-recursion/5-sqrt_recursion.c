3a8dfdee20c61374bc9137f259d27f353bf7ebf8
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-07-05 14:43:03 -0700

    task 6

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - check the code
+ *
+ * @n : is a param
+ *
+ * Return: Always 0.
+ */
+
+int _sqrt_recursion(int n)
+{
+
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * is_prime_number - check the code
+ *
+ * @n : is a param
+ *
+ * Return: Always 0.
+ */
+
+int is_prime_number(int n)
+{
+	return (prime(n, n - 1));
+}
+/**
+ * prime - check the code
+ *
+ * @num : is a param
+ *
+ * @divide : is a param
+ *
+ * Return: Always 0.
+ */
+int prime(int num, int divide)
+{
+	if (num <= 0 || num == 1)
+	{
+		return (0);
+	}
+	else if (divide == 1)
+	{
+		return (1);
+	}
+	else if (num % divide == 0)
+	{
+		return (0);
+	}
+	else if (num % divide != 0)
+	{
+		return (prime(num, divide - 1));
+	}
+	else if (num == 2)
+	{
+		return (1);
+	}
+}
