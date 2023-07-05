474518ce33f3c467cef4ce4975f10f00bd0842be
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-07-05 14:56:44 -0700

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,14 +5,14 @@
  *
  * @n : is a param
  *
- *@num : is a param
+ * @n : is a param
  *
  *@divide : is a param
  *
  * Return: Always 0.
  */
 
-int prime(int num, int divide);
+int prime(int n, int divide);
 int is_prime_number(int n)
 {
 	return (prime(n, n - 1));
@@ -20,15 +20,15 @@ int is_prime_number(int n)
 /**
  * prime - check the code
  *
- * @num : is a param
+ * @n : is a param
  *
  * @divide : is a param
  *
  * Return: Always 0.
  */
-int prime(int num, int divide)
+int prime(int n, int divide)
 {
-	if (num <= 0 || num == 1)
+	if (n <= 0 || n == 1)
 	{
 		return (0);
 	}
@@ -36,15 +36,15 @@ int prime(int num, int divide)
 	{
 		return (1);
 	}
-	else if (num % divide == 0)
+	else if (n % divide == 0)
 	{
 		return (0);
 	}
-	else if (num % divide != 0)
+	else if (n % divide != 0)
 	{
-		return (prime(num, divide - 1));
+		return (prime(n, divide - 1));
 	}
-	else if (num == 2)
+	else if (n == 2)
 	{
 		return (1);
 	}
