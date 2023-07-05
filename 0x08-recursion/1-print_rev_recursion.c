f59dcfc64db1dc3c5fd817eb3b629b55bca3e2e1
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-07-05 12:58:45 -0700

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - check the code
+ *
+ * @s : is a param
+ *
+ * Return: Always 0.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_puts_recursion(s + 1);
+		putchar(*s);
+	}
+}
