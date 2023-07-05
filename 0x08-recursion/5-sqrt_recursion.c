a7464ff4bb1cd9fc874111de0ca65abf821e8368
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-07-05 15:16:08 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,9 +5,44 @@
  *
  * @n : is a param
  *
+ * @n : is a param
+ *
+ * @num : is a param
+ *
+ * Return: Always 0.
+ */
+int square(int n, int num);
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
+
+/**
+ * square - check the code
+ *
+ * @n : is a param
+ *
+ * @num : is a param
+ *
  * Return: Always 0.
  */
 
-int _sqrt_recursion(int n)
+int square(int n, int num)
 {
-
+	if (num * num == n)
+	{
+		return (num);
+	}
+	else if (num * num < n)
+	{
+		return (square(n, num + 1));
+	}
+	else if (n < 4 && (n != 1 && n != 0))
+	{
+		return (-1);
+	}
+	else
+	{
+		return (-1);
+	}
+}
