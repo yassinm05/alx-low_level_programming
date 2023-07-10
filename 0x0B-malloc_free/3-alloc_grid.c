7740313beb8f4c47e8c41cafd0a68e61a60706f0
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-07-10 14:44:07 -0700

    task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * **alloc_grid - check the code
+ *
+ * @width: is a param
+ *
+ * @height: is a param
+ *
+ * Return: Always 0.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	int i;
+	int j;
+	int **arr;
+
+	arr = malloc(sizeof(*arr) * width);
+	if (arr == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < width; i++)
+	{
+		arr[i] = malloc(sizeof(**arr) * height);
+		if (arr[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(arr[j]);
+			}
+			free(arr);
+			return (NULL);
+		}
+		for (j = 0; j < height; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+	return (arr);
+}
