287a7a18ceb5716b0f2fdef80bd65ba44292d5a9
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-07-10 14:18:07 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,9 +19,13 @@ char *str_concat(char *s1, char *s2)
 	int j;
 	char *s3;
 
-	if (s1 == 0 || s2 == 0)
+	if (s1 == NUll)
 	{
-		return (NULL);
+		s1 = "\0";
+	}
+	if (s2 == NUll)
+	{
+		s2 = "\0";
 	}
 	while (s1[len1] != '\0')
 	{
@@ -32,7 +36,10 @@ char *str_concat(char *s1, char *s2)
 		len2++;
 	}
 	s3 = (char *)malloc(sizeof(*s3) * (len1 + len2 + 1));
-
+	if (s3 == 0)
+	{
+		return (0);
+	}
 	for (i = 0; i < len1; i++)
 	{
 		s3[i] = s1[i];
