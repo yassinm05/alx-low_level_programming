b06d2d93f47c6c24b6a6cee33fc0b9a5845f230f
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-07-10 09:13:57 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_strdup - check the code
+ *
+ * @str : is a param
+ *
+ * Return: Always 0.
+ */
+
+char *_strdup(char *str)
+{
+	int length = 0;
+	int i;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+	while (str [length] != '\0')
+	{
+		length++;
+	}
+	char *str1 = (char *)malloc(sizeof(*str1) * (length + 1));
+
+	if(str1 == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < length; i++)
+		{
+			str1[i] = str[i];
+		}
+	}
+	return (str1);
+}
