d981407f570959394f50ffc2466eb5c7492b1f04
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-07-10 14:07:50 -0700

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *str_concat - check the code
+ *
+ * @s1: is a param
+ *
+ * @s2: is a param
+ *
+ * Return: Always 0.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int len1 = 0;
+	int len2 = 0;
+	int i;
+	int j;
+	char *s3;
+
+	if (s1 == 0 || s2 == 0)
+	{
+		return (NULL);
+	}
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
+	s3 = (char *)malloc(sizeof(*s3) * (len1 + len2 + 1));
+
+	for (i = 0; i < len1; i++)
+	{
+		s3[i] = s1[i];
+	}
+	for (j = 0; j < len2; j++)
+	{
+		s3[i + j] = s2[j];
+	}
+	s3[i + j] = '\0';
+	return (s3);
+}
