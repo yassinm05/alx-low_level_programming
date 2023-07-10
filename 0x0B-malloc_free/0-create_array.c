d28750d30fac178b8524dd62e23419169b3a8e3f
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-07-10 08:42:14 -0700

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *create_array - check the code
+ *
+ * @size : is a param
+ *
+ * @c : is a param
+ *
+ * Return: Always 0.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *str = (char *)malloc(sizeof(*str) * size);
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		str[i] = c;
+	}
+	return (str);
+}
