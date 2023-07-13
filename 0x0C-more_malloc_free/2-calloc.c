cae6b6c13fc555e0cf99f9777c3a97c26ba60fd5
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-07-13 08:57:49 -0700

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_calloc - check the code
+ *
+ * @nmemb : is a param
+ *
+ * @size : is a param
+ *
+ * Return: Always 0.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i = 0;
+	unsigned int *arr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	arr = malloc (size * nmemb);
+	if (arr == 0)
+	{
+		return (NULL);
+	}
+	while (i < nmemb)
+	{
+		arr[i] = 0;
+	}
+	return (arr);
+}
