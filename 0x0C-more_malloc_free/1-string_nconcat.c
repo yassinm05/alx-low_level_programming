0ae06713a7695284d8bca1c7098a56603e9a4ec1
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-07-13 08:34:24 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,11 +22,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == 0)
 	{
-		s1 = "\0";
+		s1 = "";
 	}
 	if (s2 == 0)
 	{
-		s2 = "\0";
+		s2 = "";
 	}
 	while (s1[length] != '\0')
 	{
