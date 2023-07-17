3e1850bde477a840bc1d0496a885ce613da719aa
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-07-17 05:40:11 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - check the code
+ *
+ * @name : is a member
+ *
+ * @age : is a member
+ *
+ * @owner : is a member
+ *
+ * Return: Always 0.
+ */
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+};
+
+#endif
