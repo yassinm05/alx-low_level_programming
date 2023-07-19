41170432c6c93275f4268e6d4df09e2f108aa0bf
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-07-19 05:35:53 -0700

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,9 +14,9 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i = 0;
+	size_t i = 0;
 
-	if (action != NULL)
+	if (action != NULL && array != NULL)
 	{
 		while (i < size)
 		{
