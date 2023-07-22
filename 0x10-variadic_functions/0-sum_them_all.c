7e8050c7d7f26c679165d2f57cf5fc43b28bd61d
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-07-22 10:56:35 -0700

    task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#inaclude "variadic_functions.h>
+
+/**
+* sum_them_all - check the code
+*
+* @n : is a param
+*
+* @... : is a param
+*
+* Return: sum .
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	int i;
+	va_list ptr;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	va_start(ptr, n);
+	for (i = 0; i < n; i++)
+	{
+		sum = va_arg(ptr, int);
+	}
+	va_end(ptr);
+	return (sum);
+}
