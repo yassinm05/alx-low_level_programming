2981a8473537e7667e1120fdb5c7b5ddb8e49011
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-07-27 14:56:51 -0700

    task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * add_node - check the code
+ *
+ * @head : is a param
+ * 
+ * @str : is a param
+ * 
+ * Return: count.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newhead;
+	unsigned int length = 0;
+	char *string;
+
+	newhead = malloc(sizeof(list_t));
+	if(head == NULL || newhead == NULL)
+	{
+		return (NULL);
+	}
+	if (str != NULL)
+	{
+		newhead->str = strdup(str);
+		if (newhead->str == NULL)
+		{
+			free(newhead);
+			return (NULL);
+		}
+		string = newhead->str;
+		while (string[length] != '\0')
+		{
+			length++;
+		}
+		newhead->len = length;
+	}
+	newhead->next = *head;
+	*head = newhead;
+	return (newhead);
+}
