755f1580273f0d404c5fed7d17ad971ff978dbf0
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-08-01 13:19:32 -0700

    task 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - check the code
+ *
+ * @head : is a param
+ *
+ * @index : is a param
+ *
+ * Return: count.
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *newhead;
+	listint_t *prevnode;
+	unsigned int i = 0;
+
+	if (node == NULL || head == NULL)
+	{
+		return (-1);
+	}
+	if (index == 0)
+	{
+		newhead = *head;
+		*head = (*head)->next;
+		free(newhead);
+		return (1);
+	}
+	newhead = *head;
+	while (newhead != NULL)
+	{
+		if (i == index)
+		{
+			prevnode->next = newhead->next;
+			free(node);
+			return (1);
+		}
+		i++;
+		prevnode = newhead;
+		newhead = newhead->next;
+	}
+	return (-1);
+}
