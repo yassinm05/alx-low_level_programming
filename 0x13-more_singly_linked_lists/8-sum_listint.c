049f79fb2dc31c0f2f569c96b2775e08d3e4e4c2
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-08-01 12:38:41 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,27 @@
+#include"lists.h"
+
+/**
+ * sum_listint - check the code
+ *
+ * @head : is a param
+ *
+ * Return: count.
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *node;
+	int sum = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	node = head;
+	while (node != NULL)
+	{
+		sum = sum + node->n;
+		node = node->next;
+	}
+	return (sum);
+}
