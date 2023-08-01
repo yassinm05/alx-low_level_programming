53611203f919d6baf1884ad8e293d676d07d7f60
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-08-01 12:20:23 -0700

    task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * pop_listint - check the code
+ *
+ * @head : is a param
+ *
+ * Return: count.
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *newhead;
+	int data;
+
+	if (head == NULL || *head == NULL)
+	{
+		return (0);
+	}
+	newhead = (*head)->next;
+	data = (*head)->n;
+	free(*head);
+	free(head);
+	return (data);
+}
