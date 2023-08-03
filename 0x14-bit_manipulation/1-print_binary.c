0145cc74c0aeb5a23828fbb632fd083c377e3f43
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-08-03 11:28:06 -0700

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_binary - check the code
+ *
+ * @n : is a param
+ *
+ * Return: binary.
+ */
+
+void print_binary(unsigned long int n)
+{
+	int num = sizeof(n) * 8;
+	int res = 0;
+
+	while (num > 0)
+	{
+		if (n & 1L << --num)
+		{
+			_putchar('1');
+			res++;
+		}
+		else if (res)
+		{
+			putchar('0');
+		}
+	}
+	if (res == 0)
+	{
+		putchar('0');
+	}
+}
