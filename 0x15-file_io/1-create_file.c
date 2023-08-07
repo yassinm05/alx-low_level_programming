64fec0886183d1c4f990747b26df20f290747a7e
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-08-07 13:40:36 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,26 +21,35 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	while (str[len] != '\0')
+	if (text_content == NULL)
 	{
-		len++;
-	}
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
-	{
-		return (-1);
-	}
-	if (len != 0)
-	{
-		count = write(fd, text_content, len);
-	}
-	close(fd);
-	if (count == len)
-	{
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+		close(fd);
 		return (1);
 	}
 	else
 	{
-		return (-1);
+		while (str[len] != '\0')
+		{
+			len++;
+		}
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+		if (fd == -1)
+		{
+			return (-1);
+		}
+		if (len != 0)
+		{
+			count = write(fd, text_content, len);
+		}
+		close(fd);
+		if (count == len)
+		{
+			return (1);
+		}
+		else
+		{
+			return (-1);
+		}
 	}
 }
