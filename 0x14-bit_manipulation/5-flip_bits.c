2d9c744e14d35c537943065a8d281fc1f4e4b8d2
yassinm05 <yassinmahmoudmostafa@gmail.com>
2023-09-29 12:13:04 -0700

    task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -6,7 +6,7 @@
  *  @n : is a param
  *  @m : is a param
  *
- * Return: the number of bits you would need 
+ * Return: the number of bits you would need
  * to flip to get from one number to another
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
