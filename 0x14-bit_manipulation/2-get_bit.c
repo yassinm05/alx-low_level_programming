f5366c046e39087d164aaf5fd44167192106de28
yassinm05 <yassinmahmoudmostafa@gmail.com>
2023-09-29 11:35:49 -0700

    task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  get_bit - Entry point
+ *
+ *  @n : is a param
+ *  @index : is a param
+ *
+ *  return: the value of the bit at index index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+	{
+		return (-1);
+	}
+	return (n >> index & 1);
+}
