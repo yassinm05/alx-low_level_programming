34037b363128e01fd748e40511e46da53f1407f0
yassinm05 <yassinmahmoudmostafa@gmail.com>
2023-09-29 11:18:20 -0700

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_binary - Entry point
+ *
+ * @n : is a param
+ *
+ * return : the unsigned int after convert b
+ */
+void print_binary(unsigned long int n)
+{
+	int bit = sizeof(n) * 8;
+	inr cout = 0;
+
+	while (bit)
+	{
+		if (n & 1L << --bit)
+		{
+			_putchar('1');
+			cout++;
+		}
+		else if (cout)
+		{
+			_putchar('0');
+		}
+	}
+	if (!cout)
+	{
+		_putchar('0');
+	}
+}
