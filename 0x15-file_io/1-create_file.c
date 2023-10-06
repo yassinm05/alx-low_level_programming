4cfe5e9a85f4c0a3dd08947d31cf24e793f43dc2
yassinm05 <yassinmahmoudmostafa@gmail.com>
2023-10-06 10:10:35 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,8 +12,8 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	size_t count = 0;
-	size_t len = 0;
+	ssize_t count = 0;
+	ssize_t len = 0;
 	int result;
 
 	while (text_content[len] != '\0')
