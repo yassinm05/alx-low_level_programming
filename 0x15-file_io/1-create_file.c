d0925a11d8259cb7ad91fbcf5de51a2636686c58
yassinm05 <yassinmahmoudmostafa@gmail.com>
2023-10-06 10:26:40 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,9 +32,5 @@ int create_file(const char *filename, char *text_content)
 		count = write(fd, text_content, len);
 	}
 	close(fd);
-	if (count == len)
-		result = 1;
-	else
-		result = -1;
-	return (result);
+	return (cout == len ? 1 : -1);
 }
