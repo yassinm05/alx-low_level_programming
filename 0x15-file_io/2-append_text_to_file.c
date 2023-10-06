1a8ce8ab408ba25f98b1fb5a347b9638313bf599
yassinm05 <yassinmahmoudmostafa@gmail.com>
2023-10-06 11:03:41 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -29,5 +29,5 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (len)
 		count = write(fd, text_content, len);
 	close(fd);
-	return (bytes == len ? 1 : -1);
+	return (count == len ? 1 : -1);
 }
