d48fdcc78e81294d80a06c0e786b791c56f26f28
yassinm05 <yassinmahmoudmostafa@gmail.com>
2023-10-26 09:11:31 -0700

    task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,9 +10,7 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	int num = (!!(*n |= 1L << index));
-
 	if (index >= sizeof(n) * 8)
 		return (-1);
-	return (num);
+	return (!!(*n |= 1L << index));
 }
