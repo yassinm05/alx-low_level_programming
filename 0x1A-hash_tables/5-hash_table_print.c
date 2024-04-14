4ef3465d5dd295364181a1bef45645766ae2d4ca
yassinm05 <yassinmahoudmostafa@gmail.com>
2024-04-14 08:08:31 -0700

    task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - function prints the hashtable
+ *
+ * @ht:the hashtable
+ *
+ * Return:void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *tmp;
+	unsigned long int i;
+	int flag = 0;
+
+	if (ht == NULL || ht->array == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp != NULL)
+		{
+			if (flag == 1)
+				printf(",");
+			printf("'%s' : '%s'", tmp->key, tmp->value);
+			flag = 1;
+			tmp = tmp->next;
+		}
+	}
+	printf(")\n");
+}
