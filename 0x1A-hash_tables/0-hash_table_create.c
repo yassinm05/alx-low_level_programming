0b27cfc3c58fccbf184f2acc0abbcb6c30ac383e
yassinm05 <yassinmahoudmostafa@gmail.com>
2024-04-14 04:46:24 -0700

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!hash_table)
 		return (NULL);
 	hash_table->size = size;
-	hash_table->array = malloc(size * sizeof(hash_node_t));
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);
