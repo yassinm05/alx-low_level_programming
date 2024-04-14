f0c37fb2a1b45481c56d005848378b1afa2a4f4e
yassinm05 <yassinmahoudmostafa@gmail.com>
2024-04-14 05:45:51 -0700

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,37 @@
 #include "hash_tables.h"
+#include "hash_tables.h"
+
+/**
+ * make_hash_node - create a new hash node
+ *
+ * @key: is the key
+ * @value: value of the node
+ *
+ * Return: the new node
+ */
+hash_node_t *make_hash_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+	node->next = NULL;
+	return (node);
+}
 /**
  * hash_table_set - adds to the hashtable
  *
